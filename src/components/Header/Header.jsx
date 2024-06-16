aad1c365f8504c24ef1fdc27a2fd4b4360d0bf35
ranudev <ranu24810@gmail.com>
2024-06-16 11:27:37 +0530

    todolist2 with new ui commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div
+        style={{
+          height: "10vh",
+          width: "100vw",
+          backgroundColor: "white",
+          color: "black",
+          fontSize: "2.2em",
+          fontWeight: "400",
+          marginTop: "0px",
+          padding: "0px",
+          paddingLeft: "15%",
+          paddingTop: "1%",
+        }}
+      >
+        To-Do List Web App
+      </div>
+    </>
+  );
+}
+
+export default Header;
